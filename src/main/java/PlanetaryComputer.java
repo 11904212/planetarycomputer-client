c1ef3ff472583a9f2a5fca4d35a7f8d646e300ab
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-03-31 10:44:19 +0200

    first working request based on geotools

--- src/main/java/PlanetaryComputer.java
+++ src/main/java/PlanetaryComputer.java
@@ -2,40 +2,45 @@ import at.ac.tuwien.ba.stac.client.Item;
 import at.ac.tuwien.ba.stac.client.ItemCollection;
 import at.ac.tuwien.ba.stac.client.QueryParameter;
 import at.ac.tuwien.ba.stac.client.StacClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.geosolutions.imageio.core.BasicAuthURI;
 import it.geosolutions.imageio.plugins.cog.CogImageReadParam;
 import it.geosolutions.imageioimpl.plugins.cog.CogImageInputStreamSpi;
-import it.geosolutions.imageioimpl.plugins.cog.CogImageReader;
 import it.geosolutions.imageioimpl.plugins.cog.CogImageReaderSpi;
 import it.geosolutions.imageioimpl.plugins.cog.CogSourceSPIProvider;
 import it.geosolutions.imageioimpl.plugins.cog.HttpRangeReader;
-import mil.nga.sf.Geometry;
 import mil.nga.sf.geojson.FeatureCollection;
-import mil.nga.sf.geojson.Polygon;
-import mil.nga.sf.geojson.Position;
+//import mil.nga.sf.geojson.Polygon;
+import mil.nga.sf.geojson.Geometry;
 import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.coverage.grid.io.AbstractGridFormat;
-import org.geotools.coverage.grid.io.GridCoverage2DReader;
-import org.geotools.coverage.grid.io.GridFormatFinder;
+import org.geotools.coverage.processing.CoverageProcessor;
+import org.geotools.coverage.processing.Operations;
 import org.geotools.gce.geotiff.GeoTiffReader;
-import org.geotools.util.factory.Hints;
+import org.geotools.gce.geotiff.GeoTiffWriter;
+import org.geotools.geometry.Envelope2D;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 import org.opengis.geometry.Envelope;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class PlanetaryComputer {
 
@@ -49,9 +54,15 @@ public class PlanetaryComputer {
     }
 
 
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, ParseException, FactoryException, TransformException {
+
+        System.setProperty("org.geotools.referencing.forceXY", "true");
 
         var pcClient = new PlanetaryComputer();
+
+        long start, stop;
+
+        var geom = pcClient.getJtsGeometry();
 
         URL stacEndpoint = new URL("https://planetarycomputer.microsoft.com/api/stac/v1/");
         StacClient client = new StacClient(stacEndpoint);
@@ -59,6 +70,71 @@ public class PlanetaryComputer {
         QueryParameter queryParameter = new QueryParameter();
         queryParameter.addCollection("sentinel-2-l2a");
         queryParameter.setDatetime("2022-02-13/2022-04-15");
+        queryParameter.setIntersects(pcClient.getGeometry());
+
+        start = System.currentTimeMillis();
+        ItemCollection searchRes = client.search(queryParameter);
+        stop = System.currentTimeMillis();
+        System.out.printf("searchRes took %dms%n", stop-start);
+
+        double bestCC = Double.MAX_VALUE;
+        Item bestItem = searchRes.getItems().get(0);
+
+        for(Item item: searchRes.getItems()) {
+            double cloudCover = (double) item.getProperties().get("eo:cloud_cover");
+            if (cloudCover < bestCC) {
+                bestItem = item;
+                bestCC = cloudCover;
+            }
+
+        }
+
+        System.out.println(bestItem);
+
+        start = System.currentTimeMillis();
+        SignedLink signedLink = pcClient.signHref(bestItem.getAssets().get("visual").getHref());
+        stop = System.currentTimeMillis();
+        System.out.printf("signedLink took %dms%n", stop-start);
+
+        start = System.currentTimeMillis();
+        BasicAuthURI cogUri = new BasicAuthURI(signedLink.getHref(), false);
+        HttpRangeReader rangeReader =
+                new HttpRangeReader(cogUri.getUri(), CogImageReadParam.DEFAULT_HEADER_LENGTH);
+        CogSourceSPIProvider input =
+                new CogSourceSPIProvider(
+                        cogUri,
+                        new CogImageReaderSpi(),
+                        new CogImageInputStreamSpi(),
+                        rangeReader.getClass().getName());
+
+        GeoTiffReader reader = new GeoTiffReader(input);
+
+        GridCoverage2D coverage = reader.read(null);
+        Operations ops = new Operations(null);
+
+
+        var cropedCoverage =  (GridCoverage2D) ops.crop(coverage, geom);
+
+        File file = new File("/home/martin/Dokumente/Ausbildung/TU_Wien/ba/test_data/test_crop/test3.tif");
+        GeoTiffWriter writer = new GeoTiffWriter(file);
+        writer.write(cropedCoverage, null);
+        writer.dispose();
+
+
+        stop = System.currentTimeMillis();
+
+        System.out.printf("took %dms", stop-start);
+    }
+
+    private SignedLink signHref(String href) throws IOException {
+
+        String uriStr = SAS_ENDPOINT + "?href=" + href;
+
+        return mapper.readValue(URI.create(uriStr).toURL(), SignedLink.class);
+
+    }
+
+    private Geometry getGeometry() throws JsonProcessingException {
         String polygonStr = "{\n" +
                 "  \"type\": \"FeatureCollection\",\n" +
                 "  \"features\": [\n" +
@@ -125,61 +201,22 @@ public class PlanetaryComputer {
                 "}";
         FeatureCollection featureCollection = new ObjectMapper().readValue(polygonStr, FeatureCollection.class);
 
-        queryParameter.setIntersects(
-                featureCollection.getFeature(0).getGeometry()
-        );
 
-        ItemCollection searchRes = client.search(queryParameter);
-
-        double bestCC = Double.MAX_VALUE;
-        Item bestItem = searchRes.getItems().get(0);
-
-        for(Item item: searchRes.getItems()) {
-            double cloudCover = (double) item.getProperties().get("eo:cloud_cover");
-            if (cloudCover < bestCC) {
-                bestItem = item;
-                bestCC = cloudCover;
-            }
-
-        }
-
-        System.out.println(bestItem);
-
-        SignedLink signedLink = pcClient.signHref(bestItem.getAssets().get("visual").getHref());
-
-        System.out.println(signedLink.getHref());
-
-        URL assetURL = URI.create(signedLink.getHref()).toURL();
-        ImageInputStream cogStream = new CogImageInputStreamSpi().createInputStreamInstance(assetURL, false, null);
-
-        CogImageReader reader = new CogImageReader(new CogImageReaderSpi());
-        reader.setInput(cogStream);
-
-        CogImageReadParam param = new CogImageReadParam();
-
-        param.setSourceRegion(new Rectangle(500, 500, 100, 100));
-        param.setRangeReaderClass(HttpRangeReader.class);
-        long start = System.currentTimeMillis();
-        BufferedImage cogImage = reader.read(0, param);
-        long stop = System.currentTimeMillis();
-
-        System.out.printf("took %dms", stop-start);
-
-
+        return featureCollection.getFeature(0).getGeometry();
     }
 
-    private SignedLink signHref(String href) throws IOException {
+    private Polygon getJtsGeometry() throws ParseException, FactoryException, TransformException {
+        var wktPolygon = "POLYGON((15.14474246290308 48.125505654482964, 15.144740310902085 48.12568573568895, 15.144738298763158 48.12580142263035, 15.144717465597664 48.125953315247074, 15.144707779052617 48.126070165590036, 15.146010149034772 48.12623761981437, 15.147465978725792 48.126666765538296, 15.147096999632566 48.127126292374115, 15.147353624798829 48.12710164514268, 15.147549790959433 48.12704527712296, 15.147667582390431 48.12700699514024, 15.147917353797382 48.126865484239126, 15.148143481729862 48.12671663938023, 15.14825752943781 48.12663921846345, 15.148275270278493 48.12657708683634, 15.148260583963674 48.12654673081544, 15.148291524724753 48.12647923700685, 15.14830842463369 48.12640647346418, 15.148302252227976 48.1263010643032, 15.148226589489626 48.12628506800544, 15.147793095541541 48.126202158960375, 15.147416532429377 48.12611989843675, 15.14474246290308 48.125505654482964))";
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
+        WKTReader reader = new WKTReader(geometryFactory);
 
-        String uriStr = SAS_ENDPOINT + "?href=" + href;
+        var polygon = (Polygon) reader.read(wktPolygon);
 
-        return mapper.readValue(URI.create(uriStr).toURL(), SignedLink.class);
+        CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326");
+        CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:32633");
+        MathTransform mathTransform = CRS.findMathTransform(sourceCRS, targetCRS);
+        Polygon p1 = (Polygon) JTS.transform(polygon, mathTransform);
 
-    }
-
-    private double[] calcBbox(Polygon polygon) {
-        double[] bbox = new double[4];
-        polygon.getCoordinates();
-
-        return bbox;
+        return p1;
     }
 }
