8cb4b35a328961b1064398ae6a595a035932441c
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-08-11 15:48:28 +0200

    refactor: make SasToken immutable

--- src/main/java/at/ac/tuwien/ba/pcc/dto/SasToken.java
+++ src/main/java/at/ac/tuwien/ba/pcc/dto/SasToken.java
@@ -1,32 +1,32 @@
 package at.ac.tuwien.ba.pcc.dto;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SasToken {
-    private String token;
-    private ZonedDateTime msftExpiry;
+    private final String token;
+    private final ZonedDateTime msftExpiry;
+
+    @JsonCreator
+    public SasToken(
+            @JsonProperty(value = "token", required = true) String token,
+            @JsonProperty(value = "msft:expiry", required = true) String msftExpiry
+    ) {
+        this.token = token;
+        this.msftExpiry = ZonedDateTime
+                .parse(msftExpiry, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                .withZoneSameInstant(ZoneId.of("UTC"));
+    }
 
     public String getToken() {
         return token;
     }
 
-    @JsonSetter("token")
-    public void setToken(String token) {
-        this.token = token;
-    }
-
     public ZonedDateTime getMsftExpiry() {
         return msftExpiry;
-    }
-
-    @JsonSetter("msft:expiry")
-    public void setMsftExpiry(String msftExpiry) {
-        this.msftExpiry = ZonedDateTime
-                .parse(msftExpiry, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                .withZoneSameInstant(ZoneId.of("UTC"));
     }
 }
